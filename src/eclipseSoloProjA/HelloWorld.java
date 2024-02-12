df4e2c4ce92c46e1b3c8ad1842af9dc2f14f373d
IzzyS <isutherb@emich.edu>
2024-02-12 10:47:39 -0500

    Added second line to HelloWord.java

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,8 +3,8 @@ package eclipseSoloProjA;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-
 		System.out.println("Hello github project");
+		System.out.println(":)");
 	}
 
 }
