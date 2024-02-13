f1bbca7a2951e0f70f8563adac51f183fe5ecd42
IzzyS <isutherb@emich.edu>
2024-02-13 11:02:38 -0500

    changed line 2 in HelloWorld.java

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello github project");
-		System.out.println(":)");
+		System.out.println("edited in the alternate workspace");
 	}
 
 }
