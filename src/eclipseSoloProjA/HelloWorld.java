9f0ae27dc55c4d5557fbdb0be18d8cf710795a19
IzzyS <isutherb@emich.edu>
2024-02-13 11:12:50 -0500

    made changes to hello world

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello github project");
-		System.out.println("edited in the alternate workspace");
+		System.out.println("edited in the alternate and original workspace");
 	}
 
 }
